c9c8935745db8aeaf57e029ce1f2b59cac2f87de
Pranay KR Piyush <30485538+pranaykrpiyush@users.noreply.github.com>
2020-03-28 17:10:29 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main
+{
+ public static void main(String[] args)
+ {
+  Scanner myObj= new Scanner(System.in);
+   String name, city, sex;
+   byte age;
+   float height;
+   boolean married;
+    double pop;
+   name=myObj.nextLine();
+    city= myObj.nextLine();
+    age= myObj.nextByte();
+    height= myObj.nextFloat();
+    married= myObj.nextBoolean();
+    sex= myObj.nextLine();
+    pop = myObj.nextDouble();
+   System.out.println("Name:" + name);
+   System.out.println("\nCity:" + city);
+   System.out.println("\nAge:" + age);
+   System.out.println("\nHeight:" + height);
+   System.out.println("\nMarried:" + married);
+   System.out.println("\nSex:" + sex);
+   System.out.println("\nCity Population:" + pop);
+ }
+}
